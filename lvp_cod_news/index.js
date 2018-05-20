2c4361060ae85fc1ef7287ed99eab59b606b5592
Juangmar <koldur.c@gmail.com>
2018-05-20 17:12:56 +0200

    Now there's no messy log
    The log has only the updated bot's info and the last event displayed.

--- lvp_cod_news/index.js
+++ lvp_cod_news/index.js
@@ -6,22 +6,21 @@
 *	https://github.com/Juangmar
 *
 */
-
 var Twit = require('twit') //Loading twitter API
 var T = new Twit({
 	//Our app credentials. Obtained at https://apps.twitter.com/
-	consumer_key: '+++',
-	consumer_secret: '+++',
-	access_token: '+++',
-	access_token_secret: '+++',
+	consumer_key: '***',
+	consumer_secret: '***',
+	access_token: '***',
+	access_token_secret: '***',
 })
 var users; //Array of users IDs
 var sec = 0; //Integer variable to count how many seconds it takes to get the users.
 
 console.log("Booting up...... success!"); //Initial message. The app is running.
 console.log("Loading IDs..."); // Message: The loading of users begin.
-reloadUsers(); //User first load begin
-checkVariable(); //Method to wait for the loading to finish
+reloadUsers(); //User load begin
+checkVariable(); //Method to wait for the loading
 
 var stream = T.stream('statuses/filter', {follow: users}); //Variable that stores the twitter timeline.
 
@@ -33,7 +32,7 @@ function checkVariable() {
 		process.stdout.write("IDs loaded after " + sec + " seconds! \n"); //Print how many seconds it took.
 		stream = T.stream('statuses/filter', {follow: users}); //Replace the stream with the new user list.
 		listen(); //Proceed to listen to the TL.
-		setTimeout(checkUsers, 600000); //Check once in a while the list of users. In this case, 15 min (600.000 ms)
+		checkUsers(); //Reimport users every 15min.
 	} else{
 		//The variable is not loaded yet.
 		//Reemplazamble console message. Each time this line is called, the console line overrides.
@@ -43,37 +42,51 @@ function checkVariable() {
 		setTimeout(checkVariable, 1000); //Wait for 1 second, then call for the check function again		
 	}
 }
- 
 
 /*
 *	Starts the TL listening. It's the main function.
 */
 function listen(){
+	var openDay = new Date(); //Get the day + hour the tweet is detected.
+	var openDate =  openDay.getFullYear() + '/' + (openDay.getMonth()+1) + '/' + openDay.getDate(); //String containing the date. Format "yyyy-mm-dd"
+	var openHour = + openDay.getHours() + ":" + openDay.getMinutes(); //String containig the hour. Format "[ HH:MM:SS ] " 
+	var originalTime = openDate + " (" + openHour + ")";
+
+
 	console.log("using the following users (" + users.length + "):"); 	// To check if the loading is correct, 
 	console.log(users);													// the list is printed.
 	console.log("Now listening twitter accounts............"); //The bot really starts now.
-	var code = 0; // The initial code for the tweet that will be RTed.
+	var rt = 0; // To store the number of rts done
+	var listened = 0; // Number of tweets detected
+	var title = " ---- LVP NEWS BOT ----\n\n" // String containing the bot title
+	var lastEvent = ""; //To store the last event
 	//Stream. Each time a tweet is detected, execute the code below.
 	stream.on('tweet', function (tweet) {
 		try{
+				listened++; //One more tweet detected
 				var today = new Date(); //Get the day + hour the tweet is detected.
-				var date =  today.getFullYear() + '_' + (today.getMonth()+1) + '_' + today.getDate(); //String containing the date. Format "yyyy-mm-dd"
-				var filename = "log" + "\"" + date + ".txt"; //Log file name. It's in the log directory, with the name log\yyyy-mm-dd.txt
+				//var date =  today.getFullYear() + '_' + (today.getMonth()+1) + '_' + today.getDate(); //String containing the date. Format "yyyy-mm-dd"
+				//var filename = "log" + "\"" + date + ".txt"; //Log file name. It's in the log directory, with the name log\yyyy-mm-dd.txt
 				var hour = "[ " + today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds() + " ] "; //String containig the hour. Format "[ HH:MM:SS ] " 
 			if (users.indexOf(parseInt(tweet.user.id)) > -1){ //If the author of the tweet is in the user list:
 				// Show on log (console) the info of the tweet detected, and inform that the RT is on process.
-				// [ HH:MM:SS ] @user_name just tweeted! Now retweeting... {code: 12345}
-				console.log(hour + "@" + tweet.user.screen_name + " just tweeted! Now retweeting... {code: " + tweet.id + "}"); 
+				rt++; // One more rt to the counter
+				lastEvent = "@" + tweet.user.screen_name + " just tweeted! {Tweet id: " + tweet.id + "}"; //Update the last event (tweet detected, then proceded to post)
 				postTweet(tweet); //Method that RTs the tweet with the previous code.
-				code++; //Tweet RTd, so the unique code is incremented.
 			} else{ //The tweet is not from the user list
-				console.log(hour + "Tweet detected from @" + tweet.user.screen_name + " (Not followed). {Id: " + tweet.id_str + "}"); //Print log. Tweet detected but not RTed.
+				lastEvent = "Tweet detected from @" + tweet.user.screen_name + " (Not followed). {Tweet id: " + tweet.id_str + "}"; //Update the last event
 			}
+			process.stdout.write('\033c'); //Used to clean the console 
+			process.stdout.write(title + hour + "Since the bot started at "+ originalTime + ": Following " + users.length + " users | " + rt + " RTs done | " + listened + " tweets detected.\n" + 
+					"\t Last event: " + lastEvent);
+
+					//Post the current bot's info
+
 			reloadUsers();	//In case the user list is modified from the app or anywhere else, reolad the list (asyncronous).
-		}catch(err){
-			console.log("--> " + hour + "Exception catched. Rebooting");
-			reloadUsers();
-			checkVariable();
+		}catch(err){ //If there's an error during strean, reboot
+			console.log("--> " + hour + "Exception catched. Rebooting"); //Inform error
+			reloadUsers(); //Reload users
+			checkVariable(); //Paralize the program untill users ready
 		}
 	})
 }
@@ -84,25 +97,22 @@ function listen(){
 function postTweet(tweet){
 	//Post from the app, in form of RT and id, using the tweet.id_str, and executing the declared function.
 	T.post('statuses/retweet/:id', {id: tweet.id_str}, function (err,data,response){
-		if(!err){
-			var today = new Date();
-			//var date =  today.getFullYear() + '_' + (today.getMonth()+1) + '_' + today.getDate();
-			var hour = "[ " + today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds() + " ] ";
-			console.log(hour + "RT "+ tweet.id + " done!"); //Confirm the success of the {code} tweet RT.
-
-			var jsonData = JSON.stringify(tweet); //Get a JSON object representing the tweet object to save.
-			var fs = require('fs'); // Require In/Out module
-				fs.writeFile("tweetlog/tweet_" + tweet.id_str + ".json", jsonData, function(err) { //Write into tweetlog dir a tweetID.json file containing the tweet data.
-    				if (err) {
-        				console.log(err);
-   					} 
-				});
-		}else{
+		var today = new Date();
+		var date =  today.getFullYear() + '_' + (today.getMonth()+1) + '_' + today.getDate();
+		var filename = "log" + "\"" + date + ".txt";
+		var hour = "[ " + today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds() + " ]";
+		lastEvent = ("RT "+ tweet.id + " done!"); //Confirm the success of the {code} tweet RT.
+		var jsonData = JSON.stringify(tweet);
+		var fs = require('fs');
+			fs.writeFile("tweetlog/tweet_" + tweet.id_str + ".json", jsonData, function(err) {
+    			if (err) {
+        			console.log(err);
+   				}
+			});
 		/*
-			Handle RT error
+			WARNING: IN THE NEXT VERSION THIS FUNCTION WILL HAVE AN IF-ELSE
+			TO PRINT THE SUCCESS OR THE ERROR.
 		*/
-		}
-		
 	})
 }
 
@@ -116,39 +126,32 @@ function reloadUsers(){
 			users = data.ids; //The users content is replaced with the obtained list in the data.ids field.
 		}else{
 			//console.log(err);
-
+			// GET application / rate_limit_status.
 			//The twitter api doasn't respond or there's an error.
-			//The standard twitter app only allows 15 petitions every 15 minutes, so this is the most common reason to get an error:
-			//"rate_limit_status".
 		}
 	})
 }
 
 function checkUsers(){
-	updateUsers(); //Call for an user's update
-	setTimeout(checkUsers, 600000); //Wait 15 min (600.000 ms) untill next check.
+	updateUsers();
+	setTimeout(checkUsers, 600000);
 }
 
 function updateUsers(){
 	//Get the friends(follows) id from the @AAAAAA user, and execute the following function.
 	T.get('friends/ids', {screen_name: 'noticias_SLO'}, function (err, data, response) {
 		if(!err){
-			var oldNumb = users.length;
-
 			users = data.ids; //The users content is replaced with the obtained list in the data.ids field.
-
-			if(users.length != oldNumb){ //This means the new list is different from the previous one. Let's log that.
-				var today = new Date(); //Get current time/date
-				var hour = "[ " + today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds() + " ] "; //Format for hour log
-				console.log(hour + "Users updated!"); //There's been an update.
-				console.log("\t New user count: " + users.length + "(last one was " + oldNumb + ")."); //And this is the new user count, compared to the last
-			}
+			var today = new Date();
+			var date =  today.getFullYear() + '_' + (today.getMonth()+1) + '_' + today.getDate();
+			var filename = "log" + "\"" + date + ".txt";
+			var hour = "[ " + today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds() + " ]";
+			console.log(hour + "Users checked!");
+			console.log("\t New user count: " + users.length);
 		}else{
 			//console.log(err);
-
+			// GET application / rate_limit_status.
 			//The twitter api doasn't respond or there's an error.
-			//The standard twitter app only allows 15 petitions every 15 minutes, so this is the most common reason to get an error:
-			//"rate_limit_status".
 		}
 	})
 }
